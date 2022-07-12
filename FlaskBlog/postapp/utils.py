22ecece232f80a17780d1cbb6e351d189e48d31e
kryshen <kryshen@mail.ru>
2022-07-12 09:53:26 +0300

    path to static changed

--- FlaskBlog/postapp/utils.py
+++ FlaskBlog/postapp/utils.py
@@ -8,7 +8,7 @@ def save_picture(form_picture):
     random_hex = token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static\images', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/images', picture_fn)
 
     output_size = (1500, 600)
     i = Image.open(form_picture)
